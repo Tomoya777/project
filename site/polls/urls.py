9254cea980052860429569ee8630f1568a23eaac
Git_you <al19063@shibaura-it.ac.jp>
2021-05-25 16:25:12 +0900

    引継ぎ
    初めてのプロジェクト

---
+++ site/polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index_templates, name="index_templates"),
+    path('login/', views.MyLoginView.as_view(), name="login"),
+    path('logout/', views.MyLogoutView.as_view(), name="logout"),
+    path('home/',views.HomeView.as_view(), name="home"),
+    path('login/create/', views.UserCreateView.as_view(),name="create"),
+    path('createok/', views.UserCreateokView.as_view(),name="createok"),
+]
