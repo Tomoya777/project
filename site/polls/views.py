a92c8722c88efaf2a40ea20295e29e7019353f94
Tomoya777 <77311003+Tomoya777@users.noreply.github.com>
2021-06-13 20:03:01 +0900

    一部ページを作成しました。

--- site/polls/views.py
+++ site/polls/views.py
@@ -21,7 +21,7 @@ class HomeView(TemplateView):
     template_name = "home.html"
 
 class UserCreateView(CreateView):
-    form_class = UserCreationForm
+    form_class = forms.UserCreationForm
     template_name = "create.html"
     success_url = reverse_lazy("createok")
 
